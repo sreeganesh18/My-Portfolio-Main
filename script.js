dd6adeda2f2d4124e16d0c8a58f43bb806d96c8b
sreeganesh18 <sreeganesh1804@gmail.com>
2026-05-30 23:30:27 +0530

    feat: integrate EmailJS for contact form submission functionality

--- script.js
+++ script.js
@@ -236,3 +236,24 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 50);
   });
 });
+  // EmailJS
+function sendEmail() {
+
+    const templateParams = {
+        name: document.querySelector("#name").value,
+        email: document.querySelector("#email").value,
+        subject: document.querySelector("#subject").value,
+        message: document.querySelector("#message").value,
+    };
+
+    emailjs
+        .send("service_59rm3um", "template_thnl2xc", templateParams)
+        .then(() => {
+            alert("Email sent successfully!");
+        })
+        .catch((error) => {
+            console.log("Error sending email:", error);
+            alert("Failed to send email. Please try again.");
+        });
+
+}
